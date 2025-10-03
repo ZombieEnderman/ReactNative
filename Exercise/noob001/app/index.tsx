749aba412fb0d0740e41e3e7daf8bf0f2752a738
ZombieEnderman <p0966959620@gmail.com>
2025-10-04 00:11:08 +0800

    update noob001.

--- Exercise/noob001/app/index.tsx
+++ Exercise/noob001/app/index.tsx
@@ -1,39 +1,48 @@
-import { Text, View, StyleSheet } from "react-native";
+import {Text, View, StyleSheet} from "react-native";
 export default function ModalScreen() {
-  return (
-    <View style={css.aa}>
-      <Text style={[css.size, css.red, css.center, css.weight, css.cc]}>
-        Python
-      </Text>
-      <Text style={[css.size, css.red, css.center, css.weight, css.aa]}>
-        JavaScript
-      </Text>
-      <Text style={[css.size, css.red, css.center, css.weight, css.aa]}>
-        Ruby
-      </Text>
-    </View>
-  );
+    return (
+        <View style={css.aa}>
+            <View style={css.aa}>
+                <Text style={[css.size, css.red, css.center, css.weight, css.cc, css.byello]}>Python</Text>
+                <Text style={[css.size, css.red, css.center, css.weight, css.bb]}>JavaScript</Text>
+                <Text style={[css.size, css.red, css.center, css.weight, css.cc, css.byello]}>Ruby</Text>
+            </View>
+            <View style={css.dd}>
+                <Text style={[css.size, css.red, css.center, css.weight, css.bb]}>C#</Text>
+                <Text style={[css.size, css.red, css.center, css.weight, css.cc]}>Java</Text>
+            </View>
+        </View>
+    );
 }
 const css = StyleSheet.create({
-  aa: {
-    flex: 1,
-  },
-  cc: {
-    flex: 2,
-  },
-  size: {
-    fontSize: 30,
-  },
-  red: {
-    color: "#ff0000",
-  },
-  center: {
-    textAlign: "center",
-  },
-  byello: {
-    backgroundColor: "#ffff00",
-  },
-  weight: {
-    fontWeight: 800,
-  },
+    aa: {
+        flex: 1,
+    },
+    bb: {
+        flex: 1,
+        backgroundColor: "#00ff00",
+    },
+    cc: {
+        flex: 2,
+        backgroundColor: "#00e1ffff",
+    },
+    dd: {
+        flex: 1,
+        flexDirection: "row",
+    },
+    size: {
+        fontSize: 30,
+    },
+    red: {
+        color: "#ff0000",
+    },
+    center: {
+        textAlign: "center",
+    },
+    byello: {
+        backgroundColor: "#ffff00",
+    },
+    weight: {
+        fontWeight: 800,
+    },
 });
