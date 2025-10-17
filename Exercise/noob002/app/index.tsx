776d93df6ecff54a558912d8340dd9b7cfa7d2ed
ZombieEnderman <p0966959620@gmail.com>
2025-10-17 15:40:31 +0800

    add new app "noob002".

---
+++ Exercise/noob002/app/index.tsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+export default function ModalScreen() {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(1);
+  return (
+    <View>
+      <Text style={css.txt}>
+        {x}{y}{x}{y}{x}{y}{x}{y}{x}{y}{x}{y}{x}{y}{x}{y}
+      </Text>
+      <Text style={css.txt}>
+        {y}{x}{y}{x}{y}{x}{y}{x}{y}{x}{y}{x}{y}{x}{y}{x}
+      </Text>
+      <Text style={css.txt}>
+        {x}{y}{x}{y}{x}{y}{x}{y}{x}{y}{x}{y}{x}{y}{x}{y}
+      </Text>
+      <Text style={css.txt}>
+        {y}{x}{y}{x}{y}{x}{y}{x}{y}{x}{y}{x}{y}{x}{y}{x}
+      </Text>
+      <TouchableOpacity style={css.ntn} onPress={() => { setX(1); setY(0) }}>
+        <Text style={css.txt}>!!!</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={css.ntn} onPress={() => { setX(0); setY(1) }}>
+        <Text style={css.txt}>???</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const css = StyleSheet.create({
+  txt: {
+    fontSize: 30,
+    marginLeft: 20
+  },
+  ntn: {
+    backgroundColor: "#00ffff",
+    width: 100,
+    margin: 20
+  }
+});
