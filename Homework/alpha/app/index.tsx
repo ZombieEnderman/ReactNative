8e6aedf4814f2b7694136d739d34c3114d3ab686
ZombieEnderman <p0966959620@gmail.com>
2025-10-17 13:50:44 +0800

    homework001 Ver.5

--- Homework/alpha/app/index.tsx
+++ Homework/alpha/app/index.tsx
@@ -3,37 +3,40 @@ import {Text, View, StyleSheet} from "react-native";
 export default function Index() {
     return (
         <View style={css.root}>
-            <View style={css.display}></View>
+            <View style={css.display}>
+                <View style={css.col}></View>
+                <Text style={[css.display,css.txt]}>0</Text>
+            </View>
             <View style={css.func}>
                 <View style={css.row}>
-                    <Text style={[css.btntxt, css.col, css.colorA]}>√</Text>
-                    <Text style={[css.btntxt, css.col, css.colorA]}>±</Text>
-                    <Text style={[css.btntxt, css.col, css.colorA]}>%</Text>
-                    <Text style={[css.btntxt, css.col, css.colorA]}>÷</Text>
+                    <Text style={[css.btntxt, css.display, css.colorA]}>√</Text>
+                    <Text style={[css.btntxt, css.display, css.colorA]}>±</Text>
+                    <Text style={[css.btntxt, css.display, css.colorA]}>%</Text>
+                    <Text style={[css.btntxt, css.display, css.colorA]}>÷</Text>
                 </View>
                 <View style={css.row}>
-                    <Text style={[css.btntxt, css.col, css.colorB]}>7</Text>
-                    <Text style={[css.btntxt, css.col, css.colorB]}>8</Text>
-                    <Text style={[css.btntxt, css.col, css.colorB]}>9</Text>
-                    <Text style={[css.btntxt, css.col, css.colorA]}>x</Text>
+                    <Text style={[css.btntxt, css.display, css.colorB]}>7</Text>
+                    <Text style={[css.btntxt, css.display, css.colorB]}>8</Text>
+                    <Text style={[css.btntxt, css.display, css.colorB]}>9</Text>
+                    <Text style={[css.btntxt, css.display, css.colorA]}>x</Text>
                 </View>
                 <View style={css.row}>
-                    <Text style={[css.btntxt, css.col, css.colorB]}>4</Text>
-                    <Text style={[css.btntxt, css.col, css.colorB]}>5</Text>
-                    <Text style={[css.btntxt, css.col, css.colorB]}>6</Text>
-                    <Text style={[css.btntxt, css.col, css.colorA]}>-</Text>
+                    <Text style={[css.btntxt, css.display, css.colorB]}>4</Text>
+                    <Text style={[css.btntxt, css.display, css.colorB]}>5</Text>
+                    <Text style={[css.btntxt, css.display, css.colorB]}>6</Text>
+                    <Text style={[css.btntxt, css.display, css.colorA]}>-</Text>
                 </View>
                 <View style={css.row}>
-                    <Text style={[css.btntxt, css.col, css.colorB]}>1</Text>
-                    <Text style={[css.btntxt, css.col, css.colorB]}>2</Text>
-                    <Text style={[css.btntxt, css.col, css.colorB]}>3</Text>
-                    <Text style={[css.btntxt, css.col, css.colorA]}>+</Text>
+                    <Text style={[css.btntxt, css.display, css.colorB]}>1</Text>
+                    <Text style={[css.btntxt, css.display, css.colorB]}>2</Text>
+                    <Text style={[css.btntxt, css.display, css.colorB]}>3</Text>
+                    <Text style={[css.btntxt, css.display, css.colorA]}>+</Text>
                 </View>
                 <View style={css.row}>
-                    <Text style={[css.btntxt, css.col, css.colorC]}>AC</Text>
-                    <Text style={[css.btntxt, css.col, css.colorB]}>0</Text>
-                    <Text style={[css.btntxt, css.col, css.colorB]}>.</Text>
-                    <Text style={[css.btntxt, css.col, css.colorC]}>=</Text>
+                    <Text style={[css.btntxt, css.display, css.colorC]}>AC</Text>
+                    <Text style={[css.btntxt, css.display, css.colorB]}>0</Text>
+                    <Text style={[css.btntxt, css.display, css.colorB]}>.</Text>
+                    <Text style={[css.btntxt, css.display, css.colorC]}>=</Text>
                 </View>
             </View>
         </View>
@@ -57,7 +60,7 @@ const css = StyleSheet.create({
         flexDirection: "row",
     },
     col: {
-        flex: 1,
+        flex: 3,
     },
     btntxt: {
         color: "#ffffff",
@@ -77,4 +80,9 @@ const css = StyleSheet.create({
     colorC: {
         backgroundColor: "#98d9f5ff",
     },
+    txt:{
+        fontSize:30,
+        textAlign:"right",
+        paddingRight:30
+    }
 });
