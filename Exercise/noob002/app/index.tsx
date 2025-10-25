47e696f7ffda4ac8e4f2ee4541b1d5d0aac1f236
ZombieEnderman <p0966959620@gmail.com>
2025-10-26 00:56:48 +0800

    讓"TouchableOpacity"元件顯示於同一行。

--- Exercise/noob002/app/index.tsx
+++ Exercise/noob002/app/index.tsx
@@ -18,24 +18,29 @@ export default function ModalScreen() {
       <Text style={css.txt}>
         {y}{x}{y}{x}{y}{x}{y}{x}{y}{x}{y}{x}{y}{x}{y}{x}
       </Text>
-      <TouchableOpacity style={css.ntn} onPress={() => { setX(1); setY(0) }}>
-        <Text style={css.txt}>!!!</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={css.ntn} onPress={() => { setX(0); setY(1) }}>
-        <Text style={css.txt}>???</Text>
-      </TouchableOpacity>
+      <View style={css.dp}>
+        <TouchableOpacity style={css.ntn} onPress={() => { setX(1); setY(0) }}>
+          <Text style={css.txt}>!!!</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={css.ntn} onPress={() => { setX(0); setY(1) }}>
+          <Text style={css.txt}>???</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const css = StyleSheet.create({
-  txt: {
-    fontSize: 30,
-    marginLeft: 20
-  },
-  ntn: {
-    backgroundColor: "#00ffff",
-    width: 100,
-    margin: 20
-  }
+    txt: {
+        fontSize: 30,
+        marginLeft: 20,
+    },
+    ntn: {
+        backgroundColor: "#00ffff",
+        width: 100,
+        margin: 20,
+    },
+    dp: {
+        flexDirection: "row",
+    },
 });
