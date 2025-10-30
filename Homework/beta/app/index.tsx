d0fb3cc0a29525c8af73f8be1d425164204c3114
ZombieEnderman <p0966959620@gmail.com>
2025-10-31 00:15:46 +0800

    doing homework...

--- Homework/beta/app/index.tsx
+++ Homework/beta/app/index.tsx
@@ -2,10 +2,14 @@ import {useState} from "react";
 import {Text, View, TouchableOpacity, StyleSheet} from "react-native";
 
 export default function Index() {
+    const [single, setsingle] = useState(0);
+    const [tens, settens] = useState(0);
+    const [hundreds, sethundreds] = useState(0);
+    const [thousands, setthousands] = useState(0);
     return (
-        <View style={css.some}>
-            <View style={css.some}>
-                <Text style={css.bigword}>0</Text>
+        <View style={css.root}>
+            <View style={[css.some, css.btn]}>
+                <Text style={[css.bigword, css.cmdtext]}>{thousands} {hundreds} {tens} {single}</Text>
             </View>
             <View style={css.most}>
                 <Text style={css.bigword}>???</Text>
@@ -15,13 +19,27 @@ export default function Index() {
 }
 
 const css = StyleSheet.create({
+    root: {
+        flex: 1,
+        padding: 20,
+    },
     some: {
         flex: 1,
+        backgroundColor: "#000000",
+        marginTop: 40,
     },
     most: {
         flex: 3,
     },
     bigword: {
-        fontSize: 30,
+        fontSize: 50,
+    },
+    cmdtext: {
+        color: "#00ff00",
+    },
+    btn: {
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 10,
     },
 });
