89c9395a697429ca22abaf2fd44f8ad3942f2117
ZombieEnderman <p0966959620@gmail.com>
2025-11-09 00:52:05 +0800

    新增清單與按鈕。

--- Exercise/noob003/app/index.tsx
+++ Exercise/noob003/app/index.tsx
@@ -1,26 +1,40 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { StyleSheet, Text, View, TouchableOpacity, FlatList } from "react-native";
 
 export default function Index() {
+  const [count, setCount] = useState(0);
   const [list, setList] = useState<number[]>([]);
-  const renderItem=()=>{
+  const addItem = () => {
+    setList([...list, count]);
+  };
+  const clearItem = () => {
+    setList([]);
+  };
+  const render = ({ item, index }) => {
     return (
-      <View></View>
-    )
+      <View style={styles.litxt}>
+        <Text style={styles.word}>{item}</Text>
+      </View>
+    );
   };
   return (
     <View style={styles.root}>
       <View style={styles.title}>
-        <Text style={styles.word}>待辦事項</Text>
+        <Text style={styles.bigword}>待辦事項</Text>
       </View>
       <View style={styles.buttons}>
-        <Text>0</Text>
+        <TouchableOpacity style={[styles.btn, styles.colorB]} onPress={addItem}>
+          <Text style={styles.word}>+</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.btn, styles.colorA]} onPress={clearItem}>
+          <Text style={styles.word}>clear</Text>
+        </TouchableOpacity>
       </View>
-      <View>
-        <FlatList 
-        data={list}
-        keyExtractor={(item,index)=>`${item} - ${index}`}
-        renderItem={renderItem}
+      <View style={styles.list}>
+        <FlatList
+          data={list}
+          keyExtractor={(item, index) => `${item} - ${index}`}
+          renderItem={render}
         />
       </View>
     </View>
@@ -33,13 +47,42 @@ const styles = StyleSheet.create({
   },
   title: {
     flex: 1,
-    alignItems: "center"
+    alignItems: "center",
+    justifyContent: "center",
   },
-  buttons:{
-    flex:2
+  buttons: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    gap: 10,
+  },
+  bigword: {
+    fontSize: 30,
+    fontWeight: "700",
+  },
+  btn: {
+    minWidth: 100,
+    alignItems: "center",
+    borderRadius: 10,
   },
   word: {
-    fontSize: 26,
-    fontWeight: "700"
-  }
+    fontSize: 20,
+  },
+  list: {
+    flex: 7,
+    padding: 20,
+  },
+  litxt: {
+    borderLeftWidth: 2,
+    borderRadius: 5,
+    margin: 5,
+    backgroundColor: "#ee0",
+  },
+  colorA: {
+    backgroundColor: "#f30",
+  },
+  colorB: {
+    backgroundColor: "#39d",
+  },
 });
