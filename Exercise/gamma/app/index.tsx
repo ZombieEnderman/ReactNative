0bbb56c952eb6a121acdf59f8a82a0fe74878e94
ZombieEnderman <p0966959620@gmail.com>
2025-11-21 15:41:58 +0800

    新增深色模式切換功能到標題中。

--- Exercise/gamma/app/index.tsx
+++ Exercise/gamma/app/index.tsx
@@ -5,6 +5,7 @@ export default function Index() {
   const [height, setHeight] = useState("0");
   const [weight, setWeight] = useState("0");
   const [bmi, setBMI] = useState("0");
+  const [isDark, setIsDark] = useState(false);
   const clear = () => {
     setHeight("");
     setWeight("");
@@ -22,35 +23,66 @@ export default function Index() {
   };
   return (
     <View style={{ flex: 1, justifyContent: "center" }}>
-      <View style={styles.main}>
-        <Text style={styles.title}>BMI計算機</Text>
-      </View>
-      <View style={styles.func}>
-        <View style={styles.height}>
-          <Text style={styles.inputText}>身高:</Text>
-          <TextInput placeholder="請輸入身高(公尺)" value={height} onChangeText={setHeight} style={styles.input} />
-        </View>
-        <View style={styles.weight}>
-          <Text style={styles.inputText}>體重:</Text>
-          <TextInput placeholder="請輸入體重(公斤)" value={weight} onChangeText={setWeight} style={styles.input} />
-        </View>
-        <View style={styles.btns}>
-          <TouchableOpacity style={styles.button} onPress={clear}>
-            <Text>清除</Text>
+      {!isDark ? (
+        <>
+          <TouchableOpacity style={light.main} onPress={() => setIsDark(!isDark)}>
+            <Text style={light.title}>BMI計算機</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={calculate}>
-            <Text>計算</Text>
+          <View style={light.func}>
+            <View style={light.height}>
+              <Text style={[light.inputText, light.white]}>身高:</Text>
+              <TextInput placeholder="請輸入身高(公尺)" value={height} onChangeText={setHeight} style={light.input} />
+            </View>
+            <View style={light.weight}>
+              <Text style={[light.inputText, light.white]}>體重:</Text>
+              <TextInput placeholder="請輸入體重(公斤)" value={weight} onChangeText={setWeight} style={light.input} />
+            </View>
+            <View style={light.btns}>
+              <TouchableOpacity style={light.button} onPress={clear}>
+                <Text style={light.white}>清除</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={light.button} onPress={calculate}>
+                <Text style={light.white}>計算</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={light.display}>
+            <Text>{bmi}</Text>
+          </View>
+        </>
+      ) : (
+        <>
+          <TouchableOpacity style={dark.main} onPress={() => setIsDark(!isDark)}>
+            <Text style={dark.title}>BMI計算機</Text>
           </TouchableOpacity>
-        </View>
-      </View>
-      <View style={styles.display}>
-        <Text>{bmi}</Text>
-      </View>
+          <View style={dark.func}>
+            <View style={dark.height}>
+              <Text style={dark.inputText}>身高:</Text>
+              <TextInput placeholder="請輸入身高(公尺)" value={height} onChangeText={setHeight} style={dark.input} />
+            </View>
+            <View style={dark.weight}>
+              <Text style={dark.inputText}>體重:</Text>
+              <TextInput placeholder="請輸入體重(公斤)" value={weight} onChangeText={setWeight} style={dark.input} />
+            </View>
+            <View style={dark.btns}>
+              <TouchableOpacity style={dark.button} onPress={clear}>
+                <Text>清除</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={dark.button} onPress={calculate}>
+                <Text>計算</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={dark.display}>
+            <Text>{bmi}</Text>
+          </View>
+        </>
+      )}
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const light = StyleSheet.create({
   main: {
     flex: 1,
     backgroundColor: "#ffff00",
@@ -69,6 +101,72 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 30,
     fontWeight: "600"
+  },
+  height: {
+    flex: 1,
+    flexDirection: "row",
+    borderRadius: 10,
+    backgroundColor: "#ff8afb",
+    marginVertical: 10,
+    paddingHorizontal: 12,
+    alignItems: "center"
+  },
+  weight: {
+    flex: 1,
+    flexDirection: "row",
+    borderRadius: 10,
+    backgroundColor: "#ff8afb",
+    marginBottom: 10,
+    paddingHorizontal: 12,
+    alignItems: "center"
+  },
+  inputText: {
+    fontSize: 20,
+    fontWeight: "500",
+    height: 40,
+    marginRight: 12
+  },
+  input: {
+    height: 52,
+  },
+  btns: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: 10,
+  },
+  button: {
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#00aa00",
+    minWidth: 80,
+  },
+  white: {
+    color: "#fff"
+  }
+});
+
+const dark = StyleSheet.create({
+  main: {
+    flex: 1,
+    backgroundColor: "#333333",
+    minWidth: 180,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  func: {
+    flex: 1,
+  },
+  display: {
+    flex: 2,
+    alignItems: "center",
+    marginTop: 20,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "600",
+    color: "#ffffff",
   },
   height: {
     flex: 1,
@@ -107,7 +205,7 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "#00aa00",
+    backgroundColor: "#aaaaaa",
     minWidth: 80,
   },
 });
