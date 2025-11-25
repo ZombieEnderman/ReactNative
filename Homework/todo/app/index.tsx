e740b2cf0f5631da5a64761f4d7406aa62ad0e30
ZombieEnderman <p0966959620@gmail.com>
2025-11-25 23:04:56 +0800

    更新todo之index.tsx。

--- Homework/todo/app/index.tsx
+++ Homework/todo/app/index.tsx
@@ -1,15 +1,31 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, FlatList } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, FlatList, TextInput } from "react-native";
 
 export default function Index() {
   return (
-    <View style={styles.root}>
+    <View style={[basic.few]}>   {/* 根標籤 */}
+      <View style={[basic.few]}></View>   {/* 標題區 */}
+      <View style={[basic.func]}></View>   {/* 功能區 */}
+      <View style={[basic.most]}></View>   {/* 項目區 */}
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  root: {
+//共通性樣式
+const basic = StyleSheet.create({
+  few: {
     flex: 1,
   },
+  func: {
+    flex: 2
+  },
+  most: {
+    flex: 5
+  },
 });
+
+//深色樣式
+const dark = StyleSheet.create({});
+
+//淺色樣式
+const light = StyleSheet.create({});
