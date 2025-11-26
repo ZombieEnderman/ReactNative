4826c9236c604fb8c91a5d1184135448ee9e9fc3
ZombieEnderman <p0966959620@gmail.com>
2025-11-26 22:09:01 +0800

    再次更新index.tsx。

--- Homework/todo/app/index.tsx
+++ Homework/todo/app/index.tsx
@@ -1,12 +1,19 @@
 import { useState } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, FlatList, TextInput } from "react-native";
+import { Ionicons, Entypo } from '@expo/vector-icons';
 
 export default function Index() {
   return (
     <View style={[basic.few]}>   {/* 根標籤 */}
       <View style={[basic.few]}></View>   {/* 標題區 */}
-      <View style={[basic.func]}></View>   {/* 功能區 */}
-      <View style={[basic.most]}></View>   {/* 項目區 */}
+      <View style={[basic.func]}>   {/* 功能區 */}
+        <Entypo name="add-to-list" size={30} color="black" />  {/* 添加 */}
+      </View>
+      <View style={[basic.most]}>   {/* 項目區 */}
+        <Entypo name="edit" size={30} color="black" />  {/* 編輯 */}
+        <Entypo name="circle-with-cross" size={30} color="black" />  {/* 刪除 */}
+        <Ionicons name="information-circle" size={30} color="black" />  {/* 詳細資訊 */}
+      </View>
     </View>
   );
 }
