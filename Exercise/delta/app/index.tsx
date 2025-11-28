46ac65c4b812bcaa3798b65eaa91657f675e307c
ZombieEnderman <p0966959620@gmail.com>
2025-11-28 21:12:09 +0800

    更新index.tsx。

--- Exercise/delta/app/index.tsx
+++ Exercise/delta/app/index.tsx
@@ -1,9 +1,41 @@
-import { Text, View } from "react-native";
+import { useState } from 'react';
+import { Text, View, TouchableOpacity, StyleSheet, Alert } from "react-native";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function Index() {
+  const [word, setWord] = useState(false);
+  const change = () => {
+    const newword = !word;
+    setWord(newword);
+    const title = newword ? ("燈亮了") : ("燈滅了");
+    const message = newword ? ("誰說你可以按的!") : ("就叫你別按了!");
+    Alert.alert(title, message, [{ text: "好喔" }, { text: "不要阿" }]);
+  }
   return (
-    <View>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={[styles.center, styles.some]}>
+      <View style={[styles.center, styles.most]}>
+        <MaterialCommunityIcons name="lightbulb" size={100} color={word ? ("#0f0") : ("#f00")} />
+      </View>
+      <TouchableOpacity style={styles.some} onPress={change}>
+        <Text style={styles.big}>別亂按!</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  center: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  some: {
+    flex: 1
+  },
+  most: {
+    flex: 2
+  },
+  big: {
+    fontSize: 30,
+    fontWeight: '700'
+  },
+});
