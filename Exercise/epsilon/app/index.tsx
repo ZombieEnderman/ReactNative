a47192226cb8f2e3df2ff47edea691b491f7756d
ZombieEnderman <p0966959620@gmail.com>
2025-12-19 20:32:58 +0800

    更新樣式。

--- Exercise/epsilon/app/index.tsx
+++ Exercise/epsilon/app/index.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, TouchableOpacity, TextInput } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Index() {
@@ -29,12 +29,14 @@ export default function Index() {
     load();
   }, []);
   return (
-    <View style={{ flex: 1, alignItems: "center", margin: 10, gap: 10 }}>
-      <Text>名稱:</Text>
-      <TextInput placeholder='取名:' style={style.it} value={title} onChangeText={setTitle} />
-      <Text style={style.word}>{display}</Text>
-      <TouchableOpacity onPress={() => setDisplay(title)}>
-        <Text>確定</Text>
+    <View style={{ flex: 1, alignItems: 'center', margin: 10, gap: 10 }}>
+      <View style={{ flexDirection: 'row', alignItems: 'center', gap: 10 }}>
+        <Text style={style.word}>名稱</Text>
+        <TextInput placeholder='取名' style={style.it} value={title} onChangeText={setTitle} />
+      </View>
+      <Text style={style.bigWord}>{display}</Text>
+      <TouchableOpacity style={style.btn} onPress={() => setDisplay(title)}>
+        <Text style={style.word}>確定</Text>
       </TouchableOpacity>
     </View>
   );
@@ -42,12 +44,26 @@ export default function Index() {
 
 const style = StyleSheet.create({
   it: {
-    borderColor: "#000",
+    borderColor: '#000',
     borderWidth: 2,
     borderRadius: 10,
     width: 250,
+    marginTop: 10
+  },
+  bigWord: {
+    fontSize: 50,
+    fontWeight: '700'
   },
   word: {
-    fontSize: 30,
+    fontSize: 20,
+    fontWeight: '500'
+  },
+  btn: {
+    backgroundColor: '#008b07ff',
+    width: 70,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10
   }
 });
